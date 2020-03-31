f493d829b16bca9b6cd5513893c27d50717ab09e
Ugly Cheng <damingniao@126.com>
2020-03-31 23:38:59 +0800

    add problem 51

---
+++ 51_n_queens.py
@@ -0,0 +1,55 @@
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        if n == 1:
+            return [["Q"]]
+        ans = []
+        permutation = [-1 for i in range(n)]
+        col = [True for i in range(n)]
+        diagonal = [True for i in range(2*n-1)]
+        diagonal_reverse = [True for i in range(2*n-1)] 
+        j = 0
+        while not (j==0 and permutation[0]==n-1):
+            need_bt = True
+            for i in range(permutation[j]+1,n):
+                if col[i] and diagonal[i+j] and diagonal_reverse[j-i]:
+                    if permutation[j] != -1:
+                        col[permutation[j]] = True  
+                        diagonal[permutation[j]+j] = True
+                        diagonal_reverse[j-permutation[j]] = True
+                    permutation[j] = i
+                    col[i] = False
+                    diagonal[i+j] = False
+                    diagonal_reverse[j-i] = False
+                    j += 1
+                    need_bt = False
+                    break
+            if j == n:
+                ans.append([["." for i in range(n)] for i in range(n)])
+                for i in range(n):
+                    ans[-1][i][permutation[i]] = "Q"
+                    ans[-1][i] = "".join(ans[-1][i])
+                j = permutation[n-1]
+                col[j] = True  
+                diagonal[n-1+j] = True
+                diagonal_reverse[n-1-j] = True 
+                permutation[n-1] = -1
+                j = n-2
+            elif need_bt or permutation[j] == n-1:
+            # back tracing
+                if permutation[j] == -1:
+                    j -= 1
+                else:
+                    col[permutation[j]] = True  
+                    diagonal[permutation[j]+j] = True
+                    diagonal_reverse[j-permutation[j]] = True 
+                    permutation[j] = -1
+                    j -= 1      
+        return ans
+
+s = Solution()
+print(s.solveNQueens(1))
+                
--- README.md
+++ README.md
@@ -381,3 +381,10 @@ The replacement must be in-place and use only constant extra memory.
 ## 50.Power(x,n)
 <br>Implement pow(x, n), which calculates x raised to the power n (x<sup>n</sup>).
 <br>Solution Code: [50_power_x_n.py](./50_power_x_n.py).
+
+## 51.N-Queens
+<br>The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other. 
+Given an integer n, return all distinct solutions to the n-queens puzzle. Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+<br>Solution Code: [51_n_queens.py](./51_n_queens.py).
+
+
